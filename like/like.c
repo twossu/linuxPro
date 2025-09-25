8db404ef8ebed31a917b7e27bf1bebec5280585b
twossu <nadao202@naver.com>
2025-09-25 12:53:33 +0900

    4주차수업

---
+++ like/like.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "like.h"
+
+#define YAGE 20
+
+int main()
+{
+	struct person p = {25, "steven"};
+	if(p.age > YAGE) /* compar */
+	printf("I like %s.\n", p.name);
+	return 0;
+}
